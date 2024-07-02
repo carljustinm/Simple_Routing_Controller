a2fd562f611bcedddd2a9b8ce32d197c84f8e436
Carl Justin <carlmasedman01@gmail.com>
2024-07-02 18:06:14 +0800

    Updated the content of the About Me page

--- app/Http/Controllers/AboutMeController.php
+++ app/Http/Controllers/AboutMeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class AboutMeController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = [
+            'title' => 'About Me',
+            'content' => 'I am Carl Justin Baydo Masedman, 20 years old, born and raised in Baguio City, Philippines. 
+            I am currently taking up a Bachelor of Science in Information Technology at the University of the Cordilleras.'
+        ];
+        return view('about_me', $data);
+    }
 }
